6d2e7867b4110fe525918fb9205c7e34e4fd5c4c
pss121106 <pss1@kent.ac.uk>
2026-02-02 13:27:01 +0000

    camel_complete

--- camel.py
+++ camel.py
@@ -1,1 +1,10 @@
-# Enter your code here
+camel_case = input("camelcase:")
+print("snake_case:", end="")
+for char in camel_case:
+    if char.isupper():
+        print("_" + char.lower() , end="")
+    else:
+        print(char,end="")
+
+if __name__ =="_main_":
+    main()
