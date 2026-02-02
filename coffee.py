fe85fc0b752b5d122709f4cf87e1bb7661aa5fde
pss121106 <pss1@kent.ac.uk>
2026-02-02 13:55:54 +0000

    coffee_complete

--- coffee.py
+++ coffee.py
@@ -1,1 +1,11 @@
-#Your code goes here
+def main():
+    amount_due = 75
+    accepted_coins = [50, 20, 10, 5]
+    while amount_due > 0:
+        print (f"Amount Due: {amount_due}p")
+        coin = int(input("Insert Coin:"))
+        if coin in accepted_coins: amount_due -= coin
+    change_owed = abs(amount_due)
+    print(f"Change Owed: {change_owed}p")
+if __name__ == "__main__":
+    main()
